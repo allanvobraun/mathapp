200c37188172abc94251c9b0970ac788955ae168
allanvobraun <allanvobraun@gmail.com>
2020-06-06 12:25:34 -0300

    Diretorio raiz

--- app/app.py
+++ app/app.py
@@ -4,7 +4,8 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
-from app import keyboard
+from app import keyboard  # nescessario
+from main import ROOT
 
 
 kivy.require('1.11.1')
@@ -27,9 +28,8 @@ class MainLayout(GridLayout):
 class MathApp(App):
 
     def build(self):
-        Builder.load_string(open('mathapp-front.kv', encoding='utf8').read(), rulesonly=True)
+        Builder.load_string(open(f'{ROOT}/app/mathapp-front.kv', encoding='utf8').read(), rulesonly=True)
 
         return MainLayout()
 
 
-MathApp().run()
---
+++ main.py
@@ -0,0 +1,7 @@
+import pathlib
+from app import app
+
+ROOT = pathlib.Path(__file__).parent.absolute()  # diretorio raiz da aplicação
+
+if __name__ == '__main__':
+    app.MathApp().run()
